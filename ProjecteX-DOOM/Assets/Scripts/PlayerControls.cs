efe80b16dfacd17463c486d148a7dd0e09ad85c9
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2026-01-27 14:27:13 +0100

    Player Controller Prototype

--- ProjecteX-DOOM/Assets/Scripts/PlayerControls.cs
+++ ProjecteX-DOOM/Assets/Scripts/PlayerControls.cs
@@ -5,48 +5,6 @@ using UnityEngine.Windows;
 [RequireComponent(typeof(PlayerInputs), typeof(CharacterController))]
 public class PlayerControls : MonoBehaviour
 {
-    public float RotationSpeed = 1.0f;
-    [Tooltip("Acceleration and deceleration")]
-    public float SpeedChangeRate = 10.0f;
-
-    [Space(10)]
-    [Tooltip("The height the player can jump")]
-    public float JumpHeight = 1.2f;
-    [Tooltip("The character uses its own gravity value. The engine default is -9.81f")]
-    public float Gravity = -15.0f;
-
-    [Space(10)]
-    [Tooltip("Time required to pass before being able to jump again. Set to 0f to instantly jump again")]
-    public float JumpTimeout = 0.1f;
-    [Tooltip("Time required to pass before entering the fall state. Useful for walking down stairs")]
-    public float FallTimeout = 0.15f;
-
-    [Header("Player Grounded")]
-    [Tooltip("Useful for rough ground")]
-    public float GroundedOffset = -0.14f;
-    [Tooltip("The radius of the grounded check. Should match the radius of the CharacterController")]
-    public float GroundedRadius = 0.5f;
-    [Tooltip("What layers the character uses as ground")]
-    public LayerMask GroundLayers;
-
-    [Header("Cinemachine")]
-    [Tooltip("The follow target set in the Cinemachine Virtual Camera that the camera will follow")]
-    public GameObject CinemachineCameraTarget;
-    [Tooltip("How far in degrees can you move the camera up")]
-    public float TopClamp = 90.0f;
-    [Tooltip("How far in degrees can you move the camera down")]
-    public float BottomClamp = -90.0f;
-
-    private float _cinemachineTargetPitch;
-
-    private float _speed;
-    private float _rotationVelocity;
-
-    private float _jumpTimeoutDelta;
-    private float _fallTimeoutDelta;
-
-    private const float _threshold = 0.01f;
-
     [SerializeField] private CinemachineCamera _playerCamera;
 
     [SerializeField] private float _moveSpeed = 10f;
@@ -62,13 +20,10 @@ public class PlayerControls : MonoBehaviour
     private PlayerInputs _playerInputs;
     private CharacterController _characterController;
 
-    private float _speedOffset = 0.1f;
-
     private Vector3 _cameraRotation;
 
     private bool _isGrounded = false;
     private float _verticalVelocity;
-    private float _terminalVelocity = 50f;
 
     private void Awake()
     {
@@ -83,7 +38,7 @@ public class PlayerControls : MonoBehaviour
 
     private void Update()
     {
-        Debug.Log(_verticalVelocity);
+        Debug.Log(_isGrounded);
         GroundedCheck();
         GravityAndJump();
         Movement();
@@ -96,10 +51,9 @@ public class PlayerControls : MonoBehaviour
 
     private void GroundedCheck()
     {
-        Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - _characterController.radius, transform.position.z);
-        float sphereRadius = _characterController.radius + _characterController.skinWidth;
+        Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - _characterController.radius - _characterController.skinWidth, transform.position.z);
 
-        _isGrounded = Physics.CheckSphere(spherePosition, sphereRadius, _groundLayers, QueryTriggerInteraction.Ignore);
+        _isGrounded = Physics.CheckSphere(spherePosition, _characterController.radius, _groundLayers, QueryTriggerInteraction.Ignore);
     }
 
     private void GravityAndJump()
@@ -109,18 +63,6 @@ public class PlayerControls : MonoBehaviour
         if (_isGrounded && _verticalVelocity < 0)
         {
             _verticalVelocity = -2f;
-        }
-
-        /* Jump not in game
-        if (_playerInputs.Jump && _isGrounded)
-        {
-            _verticalVelocity = Mathf.Sqrt((_jumpHeight / 10f) * 3f * _gravity);
-        }
-        */
-
-        if (_verticalVelocity < _terminalVelocity)
-        {
-            _verticalVelocity += _gravity * Time.deltaTime;
         }
     }
 
@@ -146,50 +88,5 @@ public class PlayerControls : MonoBehaviour
 
         _playerCamera.transform.localRotation = Quaternion.Euler(_cameraRotation.y, 0f, 0f);
         transform.localRotation = Quaternion.Euler(0f, _cameraRotation.x, 0f);
-    }
-
-    private void JumpAndGravity()
-    {
-        if (_isGrounded)
-        {
-            // reset the fall timeout timer
-            _fallTimeoutDelta = FallTimeout;
-
-            // stop our velocity dropping infinitely when grounded
-            if (_verticalVelocity < 0.0f)
-            {
-                _verticalVelocity = -2f;
-            }
-
-            // Jump
-            if (_playerInputs.Jump && _jumpTimeoutDelta <= 0.0f)
-            {
-                // the square root of H * -2 * G = how much velocity needed to reach desired height
-                _verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);
-            }
-
-            // jump timeout
-            if (_jumpTimeoutDelta >= 0.0f)
-            {
-                _jumpTimeoutDelta -= Time.deltaTime;
-            }
-        }
-        else
-        {
-            // reset the jump timeout timer
-            _jumpTimeoutDelta = JumpTimeout;
-
-            // fall timeout
-            if (_fallTimeoutDelta >= 0.0f)
-            {
-                _fallTimeoutDelta -= Time.deltaTime;
-            }
-        }
-
-        // apply gravity over time if under terminal (multiply by delta time twice to linearly speed up over time)
-        if (_verticalVelocity < _terminalVelocity)
-        {
-            _verticalVelocity += Gravity * Time.deltaTime;
-        }
     }
 }
