e9447ddd65400ea371c735dde3d8643407632fb1
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2026-03-02 13:51:48 +0100

    MainMenu Options

--- ProjecteX-DOOM/Assets/Scripts/Managers/MainMenuManager.cs
+++ ProjecteX-DOOM/Assets/Scripts/Managers/MainMenuManager.cs
@@ -1,5 +1,7 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
@@ -9,6 +11,18 @@ public class MainMenuManager : MonoBehaviour
 
     [Header("Settings")]
     [SerializeField] private string _gameSceneName = "GameScene";
+
+    [Header("Option Menu References")]
+    [SerializeField] private Slider _optSenseSlider;
+    [SerializeField] private TextMeshProUGUI _optSenseValueText;
+
+    [Header("Default Option Settings")]
+    [SerializeField] private float _defaultSensitivity = 0.1f;
+
+    [Header("PlayerPrefs Keys")]
+    [SerializeField] private string _sensitivityKey = "Sensitivity";
+
+    private float _currentSensitivity;
 
     private void Start()
     {
@@ -25,15 +39,44 @@ public class MainMenuManager : MonoBehaviour
     {
         _mainMenu.SetActive(!show);
         _optionsMenu.SetActive(show);
+
+        if (show)
+        {
+            SetOptionsMenu();
+        }
+        else
+        {
+            _currentSensitivity = _optSenseSlider.value;
+        }
     }
 
     public void StartGame()
     {
+        PlayerPrefs.SetFloat("Sensitivity", _currentSensitivity);
+
         SceneManager.LoadScene(_gameSceneName);
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void ResetOptions()
+    {
+        _optSenseSlider.value = _defaultSensitivity;
+    }
+
+    private void SetOptionsMenu()
+    {
+        _currentSensitivity = PlayerPrefs.GetFloat("Sensitivity", _defaultSensitivity);
+        _optSenseSlider.value = _currentSensitivity;
+
+        UpdateSensSliderText();
+    }
+
+    public void UpdateSensSliderText()
+    {
+        _optSenseValueText.text = (_optSenseSlider.value * 10).ToString("0.0");
     }
 }
