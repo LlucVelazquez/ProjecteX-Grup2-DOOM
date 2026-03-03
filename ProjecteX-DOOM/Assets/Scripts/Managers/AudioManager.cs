23d57f802c5cb009415a53cd3fe84f314d1fc548
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2026-03-03 13:59:33 +0100

    Audios

--- ProjecteX-DOOM/Assets/Scripts/Managers/AudioManager.cs
+++ ProjecteX-DOOM/Assets/Scripts/Managers/AudioManager.cs
@@ -1,16 +1,55 @@
 using UnityEngine;
 
+public enum SFXType
+{
+
+}
+
+public enum MusicType
+{
+    
+}
+
+[RequireComponent(typeof(AudioSource))]
 public class AudioManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static AudioManager Instance { get; private set; }
+
+    private AudioSource _musicSource;
+    private AudioSource _sfxSource;
+
+    private void Reset()
     {
-        
+        int sourcesCount = GetComponents<AudioSource>().Length;
+        for (int i = 0; i < 2 - sourcesCount; i++)
+        {
+            gameObject.AddComponent<AudioSource>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        _musicSource = GetComponents<AudioSource>()[0];
+        _sfxSource = GetComponents<AudioSource>()[1];
+    }
+
+    public void PlayeMusic(MusicType music, float volume = 1f)
+    {
+
+    }
+
+    public void PlaySFX(SFXType sfx, float volume = 1f)
+    {
+
     }
 }
